d99b81fd1d6b91f91b6997f7231bb9e1afacffc3
Kirill Kudryashov <b1nanon@mail.ru>
2023-03-15 02:13:04 +0300

    added random birthday messages

--- utils/jinja_utils.py
+++ utils/jinja_utils.py
@@ -2,12 +2,13 @@ from jinja2 import Environment, FileSystemLoader
 from random import choice
 from config import PATH_TO_TEMPLATES
 
-BIRTHDAY_TEMPLATES_NAMES = ('birthday_templates/1.txt',)
+BIRTHDAY_TEMPLATES_NAMES = ('1.txt', '2.txt', '3.txt', '4.txt')
 env = Environment(loader=FileSystemLoader(PATH_TO_TEMPLATES))
 
 
 def get_birthday_message(user_id: int, gender: str):
-    template = env.get_template(choice(BIRTHDAY_TEMPLATES_NAMES))
+    chosen_template = choice(BIRTHDAY_TEMPLATES_NAMES)
+    template = env.get_template(f'birthday_templates/{chosen_template}')
     message = template.render(user_id=user_id, gender=gender)
     return message
 
